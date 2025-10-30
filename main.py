2e4f65ee470278ceef9a9fa2572dd16b81cf4713
reiya8787 <reiyagamer@gmail.com>
2025-10-30 15:37:53 +0900

    k24010課題5

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-from src.lecture05_01 import lecture05_01
+from src.lecture05_01_k24010 import lecture05_01_k24010
 
 if __name__ == "__main__":
-    lecture05_01()
+    # 画像処理と置換のメイン関数を実行
+    
+    lecture05_01_k24010()
+    
